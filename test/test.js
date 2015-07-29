657bd406151f602be171a4b82c04e0be532c9408
Yo Yehudi <yochannah@gmail.com>
2015-07-29 12:29:35 +0100

    added empty mocha tests

---
+++ test/test.js
@@ -0,0 +1,11 @@
+var assert = require("assert");
+var dummyData = require("./dummyQuery.json");
+
+describe('', function(){
+  describe('#indexOf()', function() {
+    it('should be awesome',function() {
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+});
