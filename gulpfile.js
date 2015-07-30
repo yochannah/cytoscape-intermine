8df68a0e82a8081fce4fb23ecc83484c20e44bfe
Yo Yehudi <yochannah@gmail.com>
2015-07-30 15:39:06 +0100

    compiles js successfully

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    connect = require('gulp-connect'),
+    browserify = require('browserify'),
+    sourcemaps = require('gulp-sourcemaps'),
+    gutil = require('gulp-util'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
+    mocha = require('mocha'),
+    watchify = require('watchify');
 
-var connect = require('gulp-connect');
+/**
+ * loads a server at the specified port
+ */
 
 gulp.task('connect', function () {
   connect.server({
@@ -8,6 +18,46 @@ gulp.task('connect', function () {
   });
 });
 
+/**
+* runs tests in the specified folder
+*/
+gulp.task('mocha', function() {
+    return gulp.src(['test/*.js'], { read: false })
+        .pipe(mocha({ reporter: 'list' }))
+        .on('error', gutil.log);
+});
+
+/**
+ * watches test files and re-runs the tests if the files change
+ */
+gulp.task('watch-mocha', function() {
+    gulp.watch(['js/**', 'test/**'], ['mocha']);
+});
+
+/**
+ * Bundles JS up using browserify fort
+ */
+
+
+var b = watchify(browserify({}));
+gulp.task('js', bundle);
+
+function bundle() {
+  return b.bundle()
+    // log errors if they happen
+    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+    .pipe(source('js/main.js'))
+    // optional, remove if you don't need to buffer file contents
+    .pipe(buffer())
+    // optional, remove if you dont want sourcemaps
+    .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+       // Add transformation tasks to the pipeline here.
+    .pipe(sourcemaps.write('./')) // writes .map file
+    .pipe(gulp.dest('js/bundle.js'));
+}
+
 gulp.task('default', [
-  'connect'
+  'connect',
+//  'watch-mocha',
+  'js'
 ]);
