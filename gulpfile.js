ecf9b6da551a2e800fb49344f2bdad9f8e26a4a9
Yo Yehudi <yochannah@gmail.com>
2015-07-31 14:01:17 +0100

    ok, the js actually compiles now. It'sa bit slow to livereload, so probably needs a better dev build setup with multiple bundles, so we don't rebundle cytoscape every time since it is never changing and is the largest file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var gulp        = require('gulp'),
     less        = require('gulp-less'),
     sourcemaps  = require('gulp-sourcemaps'),
@@ -8,32 +10,39 @@ var gulp        = require('gulp'),
     browserify  = require('browserify'),
     browserSync = require('browser-sync').create(),
     source      = require('vinyl-source-stream'),
-    buffer      = require('vinyl-buffer');
+    buffer      = require('vinyl-buffer'),
+    assign      = require('lodash.assign');
 
 
-/*
- * Bundles and uglifys the JS
- */
-var b = watchify(browserify({
-  debug: true
-  }));
-gulp.task('js', bundle);
+    var customOpts = {
+      entries: ['./js/main.js'],
+        debug: true
+    };
+    var opts = assign({}, watchify.args, customOpts);
+    var b = watchify(browserify(opts));
 
-function bundle() {
-  return b.bundle()
-    // log errors if they happen
-    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
-    .pipe(source('js/main.js'))
-    // optional, remove if you don't need to buffer file contents
-    .pipe(buffer())
-    // optional, remove if you dont want sourcemaps
-    .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
-    // Add transformation tasks to the pipeline here.
-    .pipe(uglify()).on('error', gutil.log)
-    .pipe(sourcemaps.write('./')) // writes .map file
-    //uglify
-    .pipe(gulp.dest('./../dist/bundle.js'));
-}
+    // add transformations here
+    // i.e. b.transform(coffeeify);
+
+    gulp.task('js', bundle); // so you can run `gulp js` to build the file
+    b.on('update', bundle); // on any dep update, runs the bundler
+    b.on('log', gutil.log); // output build logs to terminal
+
+    function bundle() {
+      return b.bundle()
+        // log errors if they happen
+        .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+        .pipe(source('bundle.js'))
+        // optional, remove if you don't need to buffer file contents
+        .pipe(buffer())
+        // optional, remove if you dont want sourcemaps
+        .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+           // Add transformation tasks to the pipeline here.
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./')) // writes .map file
+        .pipe(gulp.dest('./dist'));
+    }
+
 
 // create a task that ensures the `js` task is complete before
 // reloading browsers
