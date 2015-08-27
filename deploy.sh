74d75e4841382ea8a0e38207c4923a9f38053446
Yo Yehudi <yochannah@gmail.com>
2015-08-27 17:50:21 +0100

    let's just add the right repo url shall we?

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ git init
 git config user.name "Travis CI"
 git config user.email "travis@fakemail.com"
 
-git remote add upstream "https://$GH_TOKEN@github.com/rust-lang/rust-by-example.git"
+git remote add upstream "https://$GH_TOKEN@github.com/yochannah/cytoscape-intermine.git"
 git fetch upstream
 git reset upstream/gh-pages
 
